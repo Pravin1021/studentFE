26f0585dcd7974ecf48393bec96a3186df63e44a
Pravin1021 <mpravin1021@gmail.com>
2023-12-19 15:42:23 +0530

    first commit

--- src/Sectiona.tsx
+++ src/Sectiona.tsx
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import { TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { Dropdown } from '@mui/base/Dropdown';
+import { Menu } from '@mui/base/Menu';
 
 
 const style = {
@@ -24,7 +27,9 @@ const Sectiona = () => {
 
     const [open, setOpen] = useState(false);
     const [dataA,setDataA]=useState("")
-    console.log(dataA);
+    const [formData,setFormData]=useState([])
+
+    console.log(formData);
     
     const handleOpen = () => {
         setOpen(true);
@@ -39,6 +44,59 @@ const Sectiona = () => {
         navigate("/Sectionb")
     }
 
+    const addFormA=async()=>{
+        try{
+          await axios.post("http://localhost:3001/FormA",{name:dataA,attendance:"",date:""})
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+    const getDataA=async()=>{
+        try{
+            const response=await axios.get("http://localhost:3001/")
+            const df=response.data
+            setFormData(df)
+        }
+        catch(err){
+            console.log(err);
+            
+        }
+    }
+
+    const date=new Date()
+    const day=date.getDate()
+    const month=date.getMonth()+1
+    const year=date.getFullYear()
+    const todayDate:any=`${day}-${month}-${year}`
+
+
+    const presentFuncA=async(dta:any)=>{
+        const todayDateA = new Date().toISOString();
+
+        try{
+            await axios.put(`http://localhost:3001/FormA/${dta._id}`,{attendance:"Present",date:todayDateA})
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+    const absentFuncA=async(dta:any)=>{
+        const todayDateA = new Date().toISOString();
+
+        try{
+            await axios.put(`http://localhost:3001/FormA/${dta._id}`,{attendance:"Absent",date:todayDateA})
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+    useEffect(()=>{
+        getDataA()
+    },[presentFuncA,absentFuncA])
 
   return (
     <>
@@ -56,9 +114,22 @@ const Sectiona = () => {
             <Box sx={{ ...style, width: 400 }}>
             <h2 style={{textAlign:"center"}}>Form for Section A</h2>  
             <TextField id="outlined-basic" label="Student Name" fullWidth variant="outlined" onChange={(e:any)=>setDataA(e.target.value)} />
-            <div style={{paddingTop:"1rem"}}><Button variant="contained">Add Name</Button></div>
+            <div style={{paddingTop:"1rem"}}><Button variant="contained" onClick={addFormA}>Add Name</Button></div>
             </Box>
         </Modal>
+        <div>
+            {`Today's Date:${todayDate}`}
+            {formData.length>0?formData.map((data:any,index)=>{
+                return(
+                    <ul key={index}>
+                    <li>{data.name} {data.attendance}</li>
+                    <Button variant="contained" style={{margin:"16px"}} onClick={()=>presentFuncA(data)}>Present</Button>
+                    <Button variant="contained" onClick={()=>absentFuncA(data)} style={{margin:"16px"}}>Absent</Button>
+                </ul>
+                )
+            }):"No data found"}
+           
+        </div>
     </>
   )
 }
--- src/Sectionb.tsx
+++ src/Sectionb.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import { TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
 
 
 const style = {
@@ -23,7 +24,7 @@ const Sectionb = () => {
 
     const [open, setOpen] = useState(false);
     const [dataB,setDataB]=useState("")
-    console.log(dataB);
+    const [formData,setFormData]=useState([])
     
     const handleOpen = () => {
         setOpen(true);
@@ -37,6 +38,63 @@ const Sectionb = () => {
     const handleA=()=>{
         navigate("/")
     }
+
+    const addFormB=async()=>{
+        try{
+          await axios.post("http://localhost:3001/FormB",{name:dataB,attendance:"",date:""})
+        }
+        catch(err){
+            console.log(err);
+        }
+        handleClose()
+    }
+
+    const getDataB=async()=>{
+        try{
+            const response=await axios.get("http://localhost:3001/Sectionb")
+            const df=response.data
+            setFormData(df)
+            console.log(df);
+        }
+        catch(err){
+            console.log(err);
+            
+        }
+    }
+
+    const date=new Date()
+    const day=date.getDate()
+    const month=date.getMonth()+1
+    const year=date.getFullYear()
+    const todayDate:any=`${day}-${month}-${year}`
+
+
+    const presentFuncB=async(dta:any)=>{
+        const todayDateA = new Date().toISOString();
+
+        try{
+            await axios.put(`http://localhost:3001/FormB/${dta._id}`,{attendance:"Present",date:todayDateA})
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+    const absentFuncB=async(dta:any)=>{
+        const todayDateA = new Date().toISOString();
+
+        try{
+            await axios.put(`http://localhost:3001/FormB/${dta._id}`,{attendance:"Absent",date:todayDateA})
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+
+    useEffect(()=>{
+        getDataB()
+    },[presentFuncB,absentFuncB])
 
 
   return (
@@ -55,9 +113,22 @@ const Sectionb = () => {
             <Box sx={{ ...style, width: 400 }}>
             <h2 style={{textAlign:"center"}}>Form for Section B</h2>  
             <TextField id="outlined-basic" label="Student Name" fullWidth variant="outlined" onChange={(e)=>setDataB(e.target.value)} />
-            <div style={{paddingTop:"1rem"}}><Button variant="contained">Add Name</Button></div>
+            <div style={{paddingTop:"1rem"}}><Button variant="contained" onClick={addFormB}>Add Name</Button></div>
             </Box>
         </Modal>
+        <div>
+            {`Today's Date:${todayDate}`}
+            {formData.length>0?formData.map((data:any,index)=>{
+                return(
+                    <ul key={index}>
+                    <li>{data.name} {data.attendance}</li>
+                    <Button variant="contained" style={{margin:"16px"}} onClick={()=>presentFuncB(data)}>Present</Button>
+                    <Button variant="contained" onClick={()=>absentFuncB(data)} style={{margin:"16px"}}>Absent</Button>
+                </ul>
+                )
+            }):"No data found"}
+           
+        </div>
     </>
   )
 }
